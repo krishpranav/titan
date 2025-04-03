4ab33790585aaaf67ffb7f9d1b26bc42b1f7a2ce
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-03 22:18:35 +0530

    tokenizer: tokenizer_train

--- tokenizer/tokenizer_train.py
+++ tokenizer/tokenizer_train.py
@@ -3,3 +3,17 @@
 @date: 03-04-2025
 @author: Krisna Pranav
 '''
+
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers
+
+tokenizer = Tokenizer(models.BPE())
+tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
+
+trainer = trainers.BpeTrainer(
+    vocab_size=50257,
+    min_frequency=2,
+    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"]
+)
+
+tokenizer.train(["data/raw/text_corpus.txt"], trainer)
+tokenizer.save("tokenizer/moondra_tokenizer.json")
